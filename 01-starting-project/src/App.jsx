1d8c4d78e54de6cef32bf9a0b6c5995e26158e55
Claudio Fernandez <Claudio.Fernandez@inixgroup.com>
2024-09-20 17:23:57 +0200

    200920241723

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,16 +1,30 @@
+import TabButton from './components/TabButton/TabButton.jsx';
+import  Header  from './components/Header/Header.jsx'
+import CoreConcept from './components/CoreConcept/CoreConcept.jsx';
+import {CORE_CONCEPTS} from './data.js'
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />      
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core concepts</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+        <section id="examples"> 
+        <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>Jsx</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
+        </section>       
       </main>
     </div>
   );
